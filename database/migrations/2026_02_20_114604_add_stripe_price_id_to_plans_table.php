6d5adb19bae21187c80be377a4289b9e8e627ab0
fabiomelodev <fabiomelodev@gmail.com>
2026-02-20 21:50:36 -0300

    fix(Plan): change type field stripe_price_id

--- database/migrations/2026_02_20_114604_add_stripe_price_id_to_plans_table.php
+++ database/migrations/2026_02_20_114604_add_stripe_price_id_to_plans_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('plans', function (Blueprint $table) {
-            $table->integer('stripe_price_id')->nullable()->after('price');
+            $table->string('stripe_price_id')->nullable()->after('price');
         });
     }
 

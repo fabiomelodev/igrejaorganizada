ccb641665b576a9410563a40ed3b4977abdf85ba
fabiomelodev <fabiomelodev@gmail.com>
2026-02-20 21:48:39 -0300

    fix: change teamseeder

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -29,6 +29,7 @@ class TeamSeeder extends Seeder
                 'slug' => 'igreja-batista-01',
                 'is_active' => 1,
                 'plan_id' => 1,
+                'stripe_id' => 'cus_U1381JB1oB4g02',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
